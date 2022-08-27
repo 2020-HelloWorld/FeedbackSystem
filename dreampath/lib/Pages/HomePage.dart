a851f361827f0350f0f89ded938d2362e9dc0afc
Ghanashyam-Bhat <ghanashyambhat6@gmail.com>
2022-08-27 13:43:02 +0530

    Registeration

---
+++ dreampath/lib/Pages/HomePage.dart
@@ -0,0 +1,109 @@
+
+import 'package:dreampath/Pages/Register.dart';
+import 'package:dreampath/Pages/SplashScreen.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white12,
+      body: SingleChildScrollView(
+        child: Expanded(
+          child: Padding(
+            padding: const EdgeInsets.only(left: 75.0,top: 50),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                //--REGISTRATION BUTTON--
+                Text(
+                  "Are you new to the counselling?\nPress the button below to register",
+                  style: TextStyle(
+                    fontWeight: FontWeight.w700,
+                    fontSize: 20
+                  ),
+                ),
+
+                Container(
+                  margin: EdgeInsets.only(top: 40),
+                  height: 60,
+                  width: 200,
+                  child: FlatButton(
+                    child: Text("Register",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+
+                      ),
+                    ),
+                    onPressed: (){
+                      Navigator.push(context,MaterialPageRoute(
+                          builder: (context)=>Register()
+                      ));
+                    },
+                  ),
+                  decoration: BoxDecoration(
+                      color: Colors.deepPurple.shade700.withOpacity(0.4),
+                      borderRadius: BorderRadius.circular(20),
+                      boxShadow: [
+                        BoxShadow(
+                            color: Colors.black26,
+                            offset: Offset(4,4),
+                            spreadRadius: 3,
+                            blurRadius: 5
+                        )
+                      ]
+                  ),
+                ),
+
+                //--FEEDBACK BUTTON--
+                SizedBox(height: 100,),
+                Text(
+                  "Are you done with counselling?\nPress the button below to give feedback",
+                  style: TextStyle(
+                      fontWeight: FontWeight.w700,
+                      fontSize: 20
+                  ),
+                ),
+
+                Container(
+                  margin: EdgeInsets.only(top: 40),
+                  height: 60,
+                  width: 200,
+                  child: FlatButton(
+                    child: Text("Feedback",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+
+                      ),
+                    ),
+                    onPressed: (){
+                      Navigator.push(context,MaterialPageRoute(
+                          builder: (context)=>SplashScreen()
+                      ));
+                    },
+                  ),
+                  decoration: BoxDecoration(
+                      color: Colors.deepPurple.shade700.withOpacity(0.4),
+                      borderRadius: BorderRadius.circular(20),
+                      boxShadow: [
+                        BoxShadow(
+                            color: Colors.black26,
+                            offset: Offset(4,4),
+                            spreadRadius: 3,
+                            blurRadius: 5
+                        )
+                      ]
+                  ),
+                ),
+
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
---
+++ dreampath/lib/Pages/Register.dart
@@ -0,0 +1,139 @@
+import 'package:flutter/material.dart';
+
+class Register extends StatefulWidget {
+  const Register({Key? key}) : super(key: key);
+
+  @override
+  State<Register> createState() => _RegisterState();
+}
+
+class _RegisterState extends State<Register> {
+  TextEditingController _email = new TextEditingController();
+  TextEditingController _name = new TextEditingController();
+  TextEditingController _ph = new TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blueAccent.withOpacity(0.7),
+      body: SingleChildScrollView(
+        child: Expanded(
+          child: Padding(
+            padding: const EdgeInsets.only(top:25.0,left: 10,right: 10),
+            child: Column(
+              children: [
+                Text("ENTER YOUR INFORMATION ",
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.w700
+                  ),
+                ),
+                SizedBox(height: 50,),
+                //--Name--
+                Container(
+                  margin: EdgeInsets.all(10),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                        border: InputBorder.none,
+                        icon: Icon(Icons.mail),
+                        labelText: "Enter Name",
+                        hintText: "Name"),
+                    controller: _name,
+                  ),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20),
+                    gradient: LinearGradient(
+                      colors: [Colors.grey.shade300, Colors.white10],
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      stops: [0.0, 0.8],
+                      tileMode: TileMode.clamp,
+                    ),
+                  ),
+                ),
+                //--Phone Number
+                Container(
+                  margin: EdgeInsets.all(10),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                        border: InputBorder.none,
+                        icon: Icon(Icons.mail),
+                        labelText: "Enter Phone Number",
+                        hintText: "Phone Number"),
+                    controller: _ph,
+                  ),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20),
+                    gradient: LinearGradient(
+                      colors: [Colors.grey.shade300, Colors.white10],
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      stops: [0.0, 0.4],
+                      tileMode: TileMode.clamp,
+                    ),
+                  ),
+                ),
+                //--Email--
+                Container(
+                  margin: EdgeInsets.all(10),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                        border: InputBorder.none,
+                        icon: Icon(Icons.mail),
+                        labelText: "Enter Email",
+                        hintText: "Email"),
+                    controller: _email,
+                  ),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20),
+                    gradient: LinearGradient(
+                      colors: [Colors.grey.shade300, Colors.white10],
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      stops: [0.0, 0.4],
+                      tileMode: TileMode.clamp,
+                    ),
+                  ),
+                ),
+
+                Container(
+                  margin: EdgeInsets.only(top: 20),
+                  height: 60,
+                  width: 200,
+                  child: TextButton(
+                    child: Text(
+                      "Sign Up",
+                      style: TextStyle(
+                        fontWeight: FontWeight.w700,
+                        fontSize: 20,
+                        color: Colors.black.withBlue(50)
+                      ),
+                    ),
+                    onPressed: () {
+
+                    },
+                  ),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20),
+                    gradient: LinearGradient(
+                      colors: [Colors.white, Colors.white10],
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      stops: [0.0, 0.4],
+                      tileMode: TileMode.clamp,
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
