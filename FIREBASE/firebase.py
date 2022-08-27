db1be7181e3a57c59b69357ae617f7824ba8bb8b
Ghanashyam Bhat <74894094+Ghanashyam-Bhat@users.noreply.github.com>
2022-08-27 11:44:44 +0530

    Delete FIREBASE directory

--- FIREBASE/firebase.py
+++
@@ -1,6 +0,0 @@
-import firebase_admin 
-from firebase_admin import credentials 
-from firebase_admin import 
-
-cred = credentials . Certificate ( "path/to/serviceAccountKey.json" ) 
-firebase_admin . initialize_app ( cred )
