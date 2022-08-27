b2aa5a31cb6e5344d2b7d95be2e56987a2b4b771
Jeevan <jeevan102002@gmail.com>
2022-08-27 11:41:39 +0530

    firebase

---
+++ firebasedb/firebase.py
@@ -0,0 +1,12 @@
+import firebase_admin 
+from firebase_admin import credentials 
+from firebase_admin import firestore
+
+cred = credentials . Certificate ( "serviceAccountKey.json" ) 
+firebase_admin.initialize_app ( cred )
+
+db=firestore.client()
+
+db.collection('persons').add({'name':'john','age':'20'})
+
+
