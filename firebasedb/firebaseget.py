9da7c80ce638b448b19884510b35206b39ba4190
Nimisha <nimisha.jsw@gmail.com>
2022-08-27 17:42:50 +0530

    Firebase

--- firebasedb/firebaseget.py
+++ firebasedb/firebaseget.py
@@ -1,12 +1,14 @@
 import firebase_admin 
 from firebase_admin import credentials 
 from firebase_admin import firestore
+import datetime
 
 cred = credentials . Certificate ( "serviceAccountKey.json" ) 
 firebase_admin.initialize_app ( cred )
 
 db=firestore.client()
 
-db.collection('persons').add({'name':'john','age':'21','gender':'male'})
-
+result=db.collection('Dreampath').document('2015').collection('Student').document("S1").get()
+for doc in db.collection('Dreampath').document('2015').collection('Student').get():
+    print(doc.to_dict())
 
